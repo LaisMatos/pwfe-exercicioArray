17a27a63d4c0222ece6d523c8a679bbe25c4352f
LAIS MATOS DE CARVALHO <21276360@CFP127ALUNO.LOCAL>
2022-02-23 17:05:05 -0300

    erro em um exercicio

---
+++ main.js
@@ -0,0 +1,32 @@
+'user strich'
+
+//declaracao de array
+const valores = [1,2,3,4,5]
+
+//CRIAR UM NOVO ARRAY COM OS VALORES AO QUADRADO
+/*const soma = (valores)=> valores*valores
+const resultado=valores.map(soma)
+console.log(resultado)*/
+
+//CRIAR UM NOVO ARRAY COM OS VALORES PARES
+/*const pares=(valores)=>valores%2==0
+const resultado = valores.filter(pares)
+console.log(resultado)*/
+
+//CALCULAR A MÉDIA DOS VALORES
+/*const media=(a,b) => a+b/5
+const resultado=valores.reduce(media,0)
+console.log(resultado)*/
+
+//CALCULAR A SOMA DOS VALORES IMPARES   //### COM ERRO
+/*const impares=(valores)=>valores%2==1
+const soma = (a,b)=> a+b
+let resultado = valores.filter(impares)
+let total= resultado.reduce(soma)
+
+console.log(resultado)*/
+
+//CALCULAR O VALOR TOTAL SABENDO QUE CADA VALOR TEVE 20% DE DESCONTO
+/*const desconto = (valores)=> valores*0.2
+const resultado=valores.map(desconto)
+console.log(resultado)*/
